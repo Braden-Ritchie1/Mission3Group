5fec632ecd3118f54b68fb5b7fab9cd2cc1786d9
Raygen Blackwell <raygen.m.black@gmail.com>
2023-01-23 13:54:14 -0700

    sup

---
+++ Mission3Group/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mission3Group
+{
+    class Support
+    {
+        // receive board
+        class Receive
+        {
+            private string[] gameBoard;
+            public void SetArray(string[] newArray)
+            {
+                gameBoard = newArray;
+            }
+
+            // print board
+            public void printArray()
+            {
+                Console.WriteLine(gameBoard);
+            }
+        }
+
+        // identify results
+
+    }
+}
